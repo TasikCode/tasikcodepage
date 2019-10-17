1a851d90ac48a61e70330f6e02960f1a2fff974d
Dagus Wijaya <dagus.wijaya@gmail.com>
2019-10-17 12:10:30 +0700

    Add view using mobirise

---
+++ application/views/home.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.10.10, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.10.10, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/tasik-code-logo1-350x150.png" type="image/x-icon">
+  <meta name="description" content="Tasikmalaya Code Enthusiast Hub">
+  
+  <title>TasikCode -  Tasikmalaya Code Enthusiast Hub</title>
+  <link rel="preload" as="style" href="<?php echo base_url();?>assets/web/mobirise-icons/mobirise-icons.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/web/mobirise-icons/mobirise-icons.css">
+  <link rel="preload" as="style" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
+  <link rel="preload" as="style" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="preload" as="style" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap-reboot.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/tether/tether.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/theme/css/style.css">
+  <link rel="preload" as="style" href="<?php echo base_url();?>assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="<?php echo base_url();?>assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rF7K3BCUoD" once="menu" id="menu1-1">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://tasikcode.xyz">
+                         <img src="<?php echo base_url();?>assets/images/tasik-code-logo1-350x150.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="https://mobirise.co"><span class="mbri-alert mbr-iconfont mbr-iconfont-btn"></span>
+                        
+                        Event</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="https://mobirise.co">
+                        <span class="mbri-search mbr-iconfont mbr-iconfont-btn"></span>
+                        About Us
+                    </a>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+</section><section class="cid-rF7LEfpNCg mbr-fullscreen mbr-parallax-background" id="header2-2">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
+                    COLLABORATIVE<br>ENVIRONMENT</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">kami percaya sesuatu yang dibangun oleh banyak komponen<br>bisa bekerja lebih baik</p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-secondary display-4" href="https://mobirise.co">JOIN US</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="mbr-section article content1 cid-rF7LIvYl8M" id="content1-3">
+    
+     
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-text col-12 mbr-fonts-style display-7 col-md-8"><p><strong>TasikHub</strong> adalah sekumpulan penggiat industri digital di Tasikmalaya yang senang berkoloni dan berkolaborasi, bersama-sama berusaha membangun ekosistem digital yang baik untuk Kota Tasikmalaya yang produktif dan punya daya saing kelas dunia. Come and join us!</p></div>
+        </div>
+    </div>
+</section>
+
+<section class="features6 cid-rF7LOt473P" id="features6-6">
+    
+    
+
+    
+    <div class="container">
+        <div class="media-container-row">
+
+            <div class="card p-3 col-12 col-md-6 col-lg-4">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-setting"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7">
+                        KOLABORASI</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       Kami percaya bahwa dengan kebersamaan, kita dapat meraih capaian yang lebih besar. Dengan menggelorakan budaya berkolaborasi, kami sedang otw menuju ekosistem digital yang tidak hanya menyenangkan, tapi juga bisa diandalkan.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-4">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-hearth"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7">
+                        TERBUKA</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       TasikCode adalah sebuah wadah terbuka, siapa saja boleh ikut nimbrung disini, sekalipun kami adalah pengrajin kode didepan komputer tapi kami juga senang bergaul dengan anak band atau fans drama korea supaya bisa terlihat normal. Join us.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-4">
+                <div class="card-img pb-3">
+                    <span class="mbr-iconfont mbri-growing-chart"></span>
+                </div>
+                <div class="card-box">
+                    <h4 class="card-title py-3 mbr-fonts-style display-7">
+                        PRODUKTIF</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       Bukan sekedar kumpul-kumpul, kami punya community project yang terus berkembang dari waktu ke waktu sebagai upaya kami untuk memberikan kontribusi positif terhadap masyarakat Tasikmalaya. Lihat Repositori GitHub
+                    </p>
+                </div>
+            </div>
+
+            
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="mbr-section content7 cid-rF7LLixhIa" id="content7-5">
+          
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-12 col-md-8">
+                <div class="media-container-row">
+                    <div class="media-content">
+                        <div class="mbr-section-text">
+                            <p class="mbr-text align-right mb-0 mbr-fonts-style display-7"><strong>TasikHub</strong> adalah tempat kami bersarang dan bertelur, disini kami membicarakan banyak hal, membangun banyak hal, untuk lingkungan yang lebih baik.<br><br>Find Us</p>
+                        </div>
+                    </div>
+                    <div class="mbr-figure" style="width: 60%;">
+                     <img src="<?php echo base_url();?>assets/images/whatsapp-image-2019-10-17-at-11.56.03-436x327.jpg" alt="Mobirise" title="">  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="cid-rF7M7Oappr" id="footer1-a">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="https://tasikcode.xyz">
+                        <img src="<?php echo base_url();?>assets/images/tasik-code-logo1-548x235.png" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">TasikHub<br>Jalan Gunung Sari No. 13,<br>Kota Tasikmalaya, Jawa Barat INDONESIA</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: hai.tasikcode@gmail.com &nbsp;&nbsp;</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Links
+                </h5>
+                <p class="mbr-text">GitHub Repo<br></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2019 TasikHub - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/tasikcode" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/pages/TasikCode" target="_blank">
+                                <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                            </a>
+                        </div>
+                        
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="<?php echo base_url();?>assets/web/<?php echo base_url();?>assets/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>assets/popper/popper.min.js"></script>
+  <script src="<?php echo base_url();?>assets/bootstrap/js/bootstrap.min.js"></script>
+  <script async src="<?php echo base_url();?>assets/tether/tether.min.js"></script>
+  <script async src="<?php echo base_url();?>assets/dropdown/js/nav-dropdown.js"></script>
+  <script async src="<?php echo base_url();?>assets/dropdown/js/navbar-dropdown.js"></script>
+  <script async src="<?php echo base_url();?>assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script async src="<?php echo base_url();?>assets/parallax/jarallax.min.js"></script>
+  <script async src="<?php echo base_url();?>assets/smoothscroll/smooth-scroll.js"></script>
+  <script async src="<?php echo base_url();?>assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
