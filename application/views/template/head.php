2731d678d47257d60dabaeecc80fdfe5a5182511
Dagus Wijaya <dagus.wijaya@gmail.com>
2019-12-03 12:39:19 +0700

    add google analytics

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -25,7 +25,16 @@
   <link rel="preload" as="style" href="<?php echo base_url();?>assets/mobirise/css/mbr-additional.css">
   <link rel="stylesheet" href="<?php echo base_url();?>assets/mobirise/css/mbr-additional.css" type="text/css">
   
-  
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153770239-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-153770239-1');
+  </script>
+
   
 </head>
 <body>
